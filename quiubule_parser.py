4b8d2726318f96eb34f5d32fb90a28629080b699
OctaG <millennium935@icloud.com>
2021-11-27 13:51:35 -0600

    añadió asignaciones (uso)

--- quiubule_parser.py
+++ quiubule_parser.py
@@ -14,7 +14,8 @@ def p_instrucciones(p):
                      | lambda'''
 
 def p_instruccion(p):
-    '''instruccion : declaraciones'''
+    '''instruccion : declaraciones
+                   | asignaciones'''
 
 def p_declaraciones(p):
     '''declaraciones : declaracion declaraciones
@@ -50,13 +51,13 @@ def p_darreglo(p):
 
 def p_darr_body(p):
     '''darr_body : darr_dato COMA darr_body
-                  | lambda''' 
+                  | lambda'''
 
 def p_darr_dato(p):
     '''darr_dato : CARACTER
                   | ENTERO
                   | BOOL
-                  | REAL'''  
+                  | REAL'''
 
 def p_dstruct(p):
     '''dstruct : CHAFIRETE CARACTER BRACKET_IZQ dstruct_body BRACKET_DER PYC'''
@@ -64,19 +65,43 @@ def p_dstruct(p):
 def p_dstruct_body(p):
     '''dstruct_body : dvar dstruct_body
                   | darreglo dstruct_body
-                  | lambda'''    
+                  | lambda'''
 
 def p_dfuncion(p):
     '''dfuncion : RIFATE CARACTER PAREN_IZQ dparams PAREN_DER BRACKET_IZQ instrucciones BRACKET_DER'''
 
 def p_dparams(p):
     '''dparams : dparam COMA dparams
-                  | lambda''' 
+                  | lambda'''
 def p_dparam(p):
-    '''dparam : CARACTER'''      
+    '''dparam : CARACTER'''
+
+def p_asignaciones(p):
+    '''asignaciones : asignacion asignaciones
+                     | lambda'''
+
+def p_asignacion(p):
+    '''asignacion : a_arreglo
+                  | a_struct
+                  | a_var'''
+
+def p_a_arreglo(p):
+                                                                   # TODO: Cambiar por DATO
+    '''a_arreglo : CARACTER CORCHETE_IZQ ENTERO CORCHETE_DER IGUAL ENTERO PYC
+                 | CARACTER CORCHETE_IZQ ENTERO CORCHETE_DER IGUAL CARACTER PYC'''
+
+def p_astruct(p):
+                                #TODO: Resolver STRUCT_ID
+                                #TODO: Cambiar por DATO
+    '''a_struct : CARACTER PUNTO CARACTER IGUAL ENTERO PYC'''
+
+def p_avar(p):
+                             # TODO: Cambiar por DATO
+    '''a_var : CARACTER IGUAL ENTERO PYC
+             | CARACTER IGUAL CARACTER PYC'''
 
 def p_lambda(p):
-    '''lambda : ''' 
+    '''lambda : '''
 
 def p_error(p):
     print("Syntax error in input!")
@@ -90,19 +115,19 @@ coso foo = 10;
 coso oo = 10.45;
 juntitos hi;
 juntitos a = {1,2,3,4,};
-chafirete b { 
+chafirete b {
     coso h = 5;
-    coso j = 3; 
+    coso j = 3;
     juntitos l = {1,2,3,4,};
     };
 rifate f(a,b,c,){
     coso m = 5;
     juntitos n = {1,2,3,4,};
-    chafirete c { 
+    chafirete c {
     coso p = 5;
     };
 }
-
+arr[0] = 10;
 '''
 parser.parse(s)
 print(memory)
